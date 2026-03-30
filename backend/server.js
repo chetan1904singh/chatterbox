1cccfe75385c2b2639537ea3e32295a52143eef2
chetan1904singh <chetan1904singh@gmail.com>
2026-03-30 23:43:05 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import dotenv from 'dotenv'
 const app=express();
 
+dotenv.config()
 app.get('/',(req,res)=>{
       res.send("app running");
 })
-
-app.listen(5000,console.log("Server started on PORT 5000"))
+app.get('/home',(req,res)=>{
+      res.send("home");
+})
+const PORT= process.nextTick.PORT || 5000;
+app.listen(PORT,console.log("Server started on PORT 5000"))
